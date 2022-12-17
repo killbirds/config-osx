60e1a612533e4b8e91b0bcd18af2c46f9984e41b
Kino <kino.009@kakaocorp.com>
2022-12-17 09:46:17 +0900

    nvim-tree keymap fix

--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -1,4 +1,3 @@
-
 require("nvim-tree").setup({
   sort_by = "name",
   update_cwd = false,
@@ -31,11 +30,13 @@ require("nvim-tree").setup({
   }
 })
 
+local api = require("nvim-tree.api")
+
 vim.keymap.set("n", "<F2>", function()
-  return require("nvim-tree").toggle(true, true)
+  return api.tree.toggle(true, true)
 end, { silent = true, desc = "toggle nvim-tree" })
 
 vim.keymap.set("n", "<F3>", function()
-  return require("nvim-tree").find_file(true)
+  return vim.cmd [[NvimTreeFindFile]]
 end, { silent = true, desc = "find_file nvim-tree" })
 
