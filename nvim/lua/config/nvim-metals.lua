98684b1ebed62662ac1e460bd8aef7ff84e601ea
Kino <kino.009@kakaocorp.com>
2024-09-09 13:39:37 +0900

    metal server: 1.3.5

--- nvim/lua/config/nvim-metals.lua
+++ nvim/lua/config/nvim-metals.lua
@@ -6,7 +6,7 @@ local metals_config = require('metals').bare_config()
 
 -- Example of settings
 metals_config.settings = {
-  serverVersion = "1.3.1",
+  serverVersion = "1.3.5",
   showImplicitArguments = true,
   ammoniteJvmProperties = {"-Xmx2G"},
   bloopSbtAlreadyInstalled = true,
